debaeb4cea1189ad4fea7337edefd687b38b44c5
janhvi-r <janvirangdal@gmail.com>
2024-09-23 18:21:04 +0530

    Arrays

---
+++ Javascript/Javascipt/script.js
@@ -0,0 +1,15 @@
+//Arrays----->
+
+let marks = [90, 82, 75, 63, 41, 36];
+marks[0] = 45;
+marks[2];
+console.log(marks);
+
+
+
+
+
+//console.log(marks[2]);
+
+// let Gods = ["Bramha", "Vishnu", "Mahesh"];
+// console.log(Gods);
