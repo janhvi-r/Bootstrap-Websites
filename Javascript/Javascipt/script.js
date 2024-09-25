75c88eebbc77bd3d4e86c55de2cd9c43d07ffd2a
janhvi-r <janvirangdal@gmail.com>
2024-09-25 15:43:46 +0530

    Looping over an array

--- Javascript/Javascipt/script.js
+++ Javascript/Javascipt/script.js
@@ -1,15 +1,38 @@
 //Arrays----->
 
-let marks = [90, 82, 75, 63, 41, 36];
-marks[0] = 45;
-marks[2];
-console.log(marks);
-
-
-
-
-
+//let marks = [90, 82, 75, 63, 41, 36];
+//marks[0] = 45;
+//marks[2];
+//console.log(marks);
 //console.log(marks[2]);
 
-// let Gods = ["Bramha", "Vishnu", "Mahesh"];
+
+
+//let Gods = ["Bramha", "Vishnu", "Mahesh"];
 // console.log(Gods);
+
+
+
+//looping over an array
+let Devi = ["Mahalakshmi", "Sarswati", "Mahakali", "Hingulambika", "Saptshrungi", "RenukaMata", "Rajrajeshwari"];
+for (let i = 0; i < Devi.length; i++) {
+    console.log(Devi[i]);
+
+}
+
+let cities = ["Hyderabad", "Mumbai", "Delhi", "Gujrat", "Pune"];
+for (let city of cities) {
+    console.log(city.toUpperCase());
+}
+
+
+//Question---->
+let Marks = [85, 97, 44, 37, 76, 60];
+
+sum = 0;
+
+for (let val of Marks) {
+    sum += val;
+}
+let avg = sum / Marks.length;
+console.log(`Average Marks of the Class = ${avg}`);
