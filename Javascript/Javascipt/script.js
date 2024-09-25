b25d736a7e023ef3cacd36dcd59d8fae8205afcb
janhvi-r <janvirangdal@gmail.com>
2024-09-25 18:03:48 +0530

    Average in array

--- Javascript/Javascipt/script.js
+++ Javascript/Javascipt/script.js
@@ -1,38 +1,60 @@
-//Arrays----->
+// //Arrays----->
 
-//let marks = [90, 82, 75, 63, 41, 36];
-//marks[0] = 45;
-//marks[2];
-//console.log(marks);
-//console.log(marks[2]);
-
-
-
-//let Gods = ["Bramha", "Vishnu", "Mahesh"];
-// console.log(Gods);
+// //let marks = [90, 82, 75, 63, 41, 36];
+// //marks[0] = 45;
+// //marks[2];
+// //console.log(marks);
+// //console.log(marks[2]);
 
 
 
-//looping over an array
-let Devi = ["Mahalakshmi", "Sarswati", "Mahakali", "Hingulambika", "Saptshrungi", "RenukaMata", "Rajrajeshwari"];
-for (let i = 0; i < Devi.length; i++) {
-    console.log(Devi[i]);
+// //let Gods = ["Bramha", "Vishnu", "Mahesh"];
+// // console.log(Gods);
+
+
+
+// //looping over an array
+// let Devi = ["Mahalakshmi", "Sarswati", "Mahakali", "Hingulambika", "Saptshrungi", "RenukaMata", "Rajrajeshwari"];
+// for (let i = 0; i < Devi.length; i++) {
+//     console.log(Devi[i]);
+
+// }
+// //i used FOR OF LOOP in array
+// let sisters = ["Yogita", "Pooja", "Jaya"];
+// for (let sis of sisters) {
+//     console.log(sis.toUpperCase());
+// }
+
+// let cities = ["Hyderabad", "Mumbai", "Delhi", "Gujrat", "Pune"];
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+// }
+
+
+// //Question---->
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let val of marks) {
+//     sum += val;
+// }
+// console.log(sum);
+
+let mar = [34, 34, 66, 78, 89, 60];
+let total = 0;
+for (let m of mar) {
+    total += m;
+}
+let avg = total / mar.length;
+console.log(total);
+console.log(avg);
+
+
+let number = [2, 2, 1, 3, 4, 5];
+let sum = 0;
+for (let num of number) {
+    sum += num;
 
 }
-
-let cities = ["Hyderabad", "Mumbai", "Delhi", "Gujrat", "Pune"];
-for (let city of cities) {
-    console.log(city.toUpperCase());
-}
-
-
-//Question---->
-let Marks = [85, 97, 44, 37, 76, 60];
-
-sum = 0;
-
-for (let val of Marks) {
-    sum += val;
-}
-let avg = sum / Marks.length;
-console.log(`Average Marks of the Class = ${avg}`);
+console.log(sum);
+let avg1 = sum / number.length;
+console.log(avg1);
